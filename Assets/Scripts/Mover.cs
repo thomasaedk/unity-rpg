a345f76cbfe90148d3c960e3cbceef30bdb8ba8a
Thomas Aegidiussen <thomasaedk@gmail.com>
2021-05-26 20:18:48 +0200

    Implement click-to-move and fixed follow camera

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,19 +5,23 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-
-    private Ray lastRay;
-    
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
-        
-        NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
-        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+            navMeshAgent.SetDestination(hit.point);
+        }
     }
 }
