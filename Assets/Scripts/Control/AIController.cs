f1f2e19f83deacb957ac9620ced217efaac7b5bd
Thomas Aegidiussen <thomasaedk@gmail.com>
2021-06-13 11:23:38 +0200

    More AI (guarding and suspicion)

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -5,22 +5,33 @@ using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
         [SerializeField] private float chaseDistance = 5f;
+        [SerializeField] private float suspicionTime = 3f;
 
         private GameObject _player;
         private Health _health;
         private Fighter _fighter;
+        private Mover _mover;
+        private ActionScheduler _actionScheduler;
 
+        private Vector3 guardPosition;
+        private float timeSinceLastSawPlayer = float.PositiveInfinity;
+        
         private void Start()
         {
             _player = GameObject.FindWithTag("Player");
             _fighter = GetComponent<Fighter>();
             _health = GetComponent<Health>();
+            _mover = GetComponent<Mover>();
+            _actionScheduler = GetComponent<ActionScheduler>();
+
+            guardPosition = transform.position;
         }
 
         private void Update()
@@ -28,12 +39,34 @@ namespace RPG.Control
             if (_health.IsDead) return;
             if (InAttackRange() && _fighter.CanAttack(_player))
             {
-                _fighter.Attack(_player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                _fighter.Cancel();
+                GuardBehaviour();
             }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void AttackBehaviour()
+        {
+            _fighter.Attack(_player);
+        }
+        
+        private void SuspicionBehaviour()
+        {
+            _actionScheduler.CancelCurrentAction();
+        }
+        
+        private void GuardBehaviour()
+        {
+            _mover.StartMoveAction(guardPosition);
         }
 
         private bool InAttackRange()
